69cf42a3e0620cf0bb91570d852c3cbfe182e91c
Jeff Hedfors <git.j@hedfors.net>
2016-04-04 16:10:32 -0700

    completed 009

--- 009GetArrayValues/index.php
+++ 009GetArrayValues/index.php
@@ -1,3 +1,18 @@
 <?php
+  echo 'Create a function called "print_lists" that takes any array and prints each value in the array in an &lt;li&gt;, which is part of a &lt;ul&gt;. For example, running print_lists($A) where $A = array(2,3,"hello"); should output a html response that looks like...<br>';
+
+function print_lists($arr){
+  echo "<ul>";
+  for ($i=0; $i < count($arr); $i++) {
+    echo "<li>$arr[$i]</li>";
+  }
+  echo "</ul>";
+}
+
+$A = array(2,3,'hello');
+
+print_lists($A);
+
+
 
 ?>
