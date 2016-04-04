340cc780c6bcb7ff8fee02103b3cd023a3c73cda
Jeff Hedfors <git.j@hedfors.net>
2016-04-04 11:33:29 -0700

    added test folder

--- 004Average/index.php
+++ 004Average/index.php
@@ -1,5 +1,6 @@
 <?php
-  echo 'Create a program that prints the average of the values in the array called "numbers". Array "numbers" should have the following values: 1, 2, 5, 10, 255, 3. <br>'
-
+  echo 'Create a program that prints the average of the values in the array called "numbers". Array "numbers" should have the following values: 1, 2, 5, 10, 255, 3. <br>';
   
+
+
  ?>
