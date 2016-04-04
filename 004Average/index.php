93ce4c35da340748b2f0a9c0ac0e46ab6f071acd
Jeff Hedfors <git.j@hedfors.net>
2016-04-04 11:54:00 -0700

    finishes sum of array values

--- 004Average/index.php
+++ 004Average/index.php
@@ -2,6 +2,12 @@
   echo 'Create a program that prints the average of the values in the array called "numbers". Array "numbers" should have the following values: 1, 2, 5, 10, 255, 3. <br>';
 
   $sum = 0;
-  
+  $numbers = [1, 2, 5, 10, 255, 3];
+
+  for ($i=0; $i < count($numbers); $i++) {
+    $sum+=$numbers[$i];
+  }
+
+  echo $sum/count($numbers);
 
  ?>
