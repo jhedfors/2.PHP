22633a4e773be7c44460d6fee7624d0bad43852f
Jeff Hedfors <git.j@hedfors.net>
2016-04-04 13:39:16 -0700

    completed 005

--- 005OddNumbersArray/index.php
+++ 005OddNumbersArray/index.php
@@ -1,3 +1,11 @@
 <?php
+  echo "Create an array that inclusively contains all odd numbers between 0 to 20,000. Use the var_dump() function at the end to display the array values without using loops. <br>";
+  $numbers = [];
+  for ($i=1; $i < 20000; $i+=2) {
+    array_push($numbers,$i);
+  }
+
+   var_dump($numbers);
+
 
  ?>
