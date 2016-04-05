d95918f8b80c8b8ca258ad8ca9a51c666f4537fb
Jeff Hedfors <git.j@hedfors.net>
2016-04-04 19:13:01 -0700

    completed 013

--- 013CoinThrow/index.php
+++ 013CoinThrow/index.php
@@ -1,3 +1,31 @@
 <?php
 
+coinToss();
+
+  function coinToss(){
+    $count = 0;
+    $heads = 0;
+    $tails = 0;
+
+    echo "<h2 style = \"text-decoration: underline;font-style:italic\">Starting the program</h2>";
+
+    for ($i=1; $i <=5000  ; $i++) {
+      $coin = rand(0,1);
+      if($coin==0){
+        $heads++;
+        $count++;
+        echo "<p>Attempt #$count: Throwing a coin... it's a head! Got $heads heads(s) so far and $tails tails(s) so far</p>";
+        }
+        else{
+        $tails++;
+        $count++;
+        echo "<p>Attempt #$count: Throwing a coin... it's a head! Got $heads heads(s) so far and $tails tails(s) so far</p>";
+      }
+    }
+    echo "<h2 style = \"text-decoration: underline;font-style:italic\">Ending the program - thank you!</h2>";
+
+}
+
+
+
 ?>
