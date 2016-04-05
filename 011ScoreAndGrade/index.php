55e9d5464a7f9827f4960032b520cf8956c1d1e4
Jeff Hedfors <git.j@hedfors.net>
2016-04-04 18:11:23 -0700

    completed 011

--- 011ScoreAndGrade/index.php
+++ 011ScoreAndGrade/index.php
@@ -1,3 +1,30 @@
 <?php
+//
+
+for ($i=0; $i < 100; $i++) {
+  $score = rand(50,100);
+  yourGrade($score);
+
+}
+function yourGrade($score){
+  if ($score < 70) {
+    echo "<h1>Your Score: $score/100</h1>";
+    echo "<h2>Your grade is D.";
+  }
+  else if ($score < 80) {
+    echo "<h1>Your Score: $score/100</h1>";
+    echo "<h2>Your grade is C.";
+  }
+  else if ($score < 90) {
+    echo "<h1>Your Score: $score/100</h1>";
+    echo "<h2>Your grade is B.";
+  }
+  else if ($score <= 100) {
+    echo "<h1>Your Score: $score/100</h1>";
+    echo "<h2>Your grade is A.";
+  }
+
+}
+
 
 ?>
