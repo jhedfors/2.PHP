21378a644dc22573984a646d5edd85b1af485237
Jeff Hedfors <git.j@hedfors.net>
2016-04-04 18:32:11 -0700

    completed 012

--- 012StatesAttay/index.php
+++ 012StatesAttay/index.php
@@ -1,3 +1,28 @@
 <?php
+  $states = array('CA', 'WA', 'VA', 'UT', 'AZ');
+
+  function dropDownForLoop($arr){
+    echo "<select>";
+    for ($i=0; $i < count($arr); $i++) {
+      echo "<option>$arr[$i]</option>";
+      // echo "$arr[i]"
+    }
+    echo "</select>";
+  }
+
+  function dropDownForeachLoop($arr){
+    echo "<select>";
+    foreach ($arr as $key => $value) {
+      echo "<option>$value</option>";
+    }
+    echo "</select>";
+  }
+
+
+  dropDownForLoop($states);
+  dropDownForeachLoop($states);
+  array_push($states,'NJ', 'NY', 'DE');
+  dropDownForeachLoop($states);
+
 
 ?>
