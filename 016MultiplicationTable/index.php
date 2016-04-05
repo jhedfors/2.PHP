191fc097cfd611f4b6161da6a55f3e35f7f450e9
Jeff Hedfors <git.j@hedfors.net>
2016-04-05 11:35:00 -0700

    initial commit of 016 and 018

--- 016MultiplicationTable/index.php
+++ 016MultiplicationTable/index.php
@@ -1,3 +1,42 @@
 <?php
 
+echo '
+<table>
+  <tr>
+     <td>1x1</td>
+     <td>1x2</td>
+     <td>1x3</td>
+  </tr>
+  <tr>
+     <td>2x1</td>
+     <td>2x2</td>
+     <td>2x3</td>
+  </tr>
+  <tr>
+     <td>3x1</td>
+     <td>3x2</td>
+     <td>3x3</td>
+  </tr>
+</table>
+';
+?>
+<?php
+echo '
+<table>';
+
+ for($i=0; $i<3; $i++)
+      { ?>
+      <tr>
+         <td><?php echo $i; ?>x1</td>
+         <td><?php echo $i; ?>x2</td>
+         <td><?php echo $i; ?>x3</td>
+      </tr>
+<?php } ?>
+echo '
+</table>
+';
+
+
+'
+
 ?>
--- 018HTMLTable/index.php
+++ 018HTMLTable/index.php
@@ -1,3 +1,98 @@
+<html>
+
+<body>
+
+  <table>
+
+
+    <thead>
+      <tr>
+        <td>
+          User #
+        </td>
+        <td>
+          First Name
+        </td>
+        <td>
+          Last Name
+        </td>
+        <td>
+          Full Name
+        </td>
+        <td>
+          Full Name in upper case
+        </td>
+        <td>
+          Length of full name (without spaces)
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+
+    $users = array(
+       array('first_name' => 'Michael', 'last_name' => 'Choi'),
+       array('first_name' => 'John', 'last_name' => 'Supsupin'),
+       array('first_name' => 'Mark', 'last_name' => 'Guillen'),
+       array('first_name' => 'KB', 'last_name' => 'Tonel')
+    );
+    // $rowNumber =1;
+    foreach ($users as $key => $row) {
+      echo "<tr class = 'row'>";
+      echo "<td class = 'usernumber'>";
+      echo $key+1;
+        echo '</td>';
+      foreach ($row as $key => $value) {
+        // echo $value;
+
+        echo "<td>$row[first_name] ";
+        // var_dump($row);
+
+        echo "</td>";
+        echo "<td>$row[last_name] ";
+        // var_dump($row);
+
+        echo "</td>";
+        echo "<td>$row[first_name] $row[last_name]";
+        // var_dump($row);
+
+        echo "</td>";
+        // $uppercase = strtoupper($row[first_name]);
+        // echo "<td>$uppercase";
+        // // var_dump($row);
+
+        echo "</td>";
+      }
+
+      echo "</tr>";
+      // $rowNumber++;
+      // var_dump($users);
+    }
+
+    ?>
+
+
+
+
+      <tr>
+        <td>
+
+        </td>
+      </tr>
+
+
+    </tbody>
+  </table>
+
+</body>
+
+
 <?php
 
+
+
+
+
 ?>
+
+</html>
