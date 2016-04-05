13355f0eb10b1e7ee3a3896e2853d2de402d7a84
Jeff Hedfors <git.j@hedfors.net>
2016-04-04 17:03:42 -0700

    completed 010

--- 010KeysAndValues/index.php
+++ 010KeysAndValues/index.php
@@ -1,3 +1,31 @@
 <?php
 
+
+  $users = array('first_name' => 'Michael', 'last_name' => 'Choi');
+  function keyNameAndValue($users){
+    foreach ($users as $key => $value) {
+      echo "The value in the key '$key' is '$value'.<br>";
+    }
+
+  }
+
+
+function describeArray($users){
+  $keyCount = 0;
+  $KeyName = '';
+
+  foreach ($users as $key => $value) {
+    $keyCount++;
+    $KeyName .= "$key, ";
+  }
+
+  echo "There are $keyCount keys in this array: $KeyName<br>" ;
+  echo keyNameAndValue($users);
+
+
+}
+
+describeArray($users);
+
+
 ?>
