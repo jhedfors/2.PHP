e9e465a59774b9008211d70b50decc273df94474
Jeff Hedfors <git.j@hedfors.net>
2016-04-08 22:53:06 -0700

    backup misc SQL assignments

---
+++ 4.2DBCommunication/home.php
@@ -0,0 +1,33 @@
+<?php
+  require("new-connection.php");
+
+
+  // $query = "SELECT * FROM people";
+  // $people = fetch_all($query);
+  // var_dump($people);
+
+  // $query = "SELECT * FROM people where id='1'";
+  // $person = fetch_record($query);
+  // var_dump($person);
+
+
+ ?>
+
+ <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      // echo $person['first_name'];
+
+      // echo fetch_record("SELECT * FROM people where id='1'")['first_name'];
+      // var_dump(fetch_all("SELECT * FROM people"));
+
+     ?>
+
+
+  </body>
+</html>
